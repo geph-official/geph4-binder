81e6c82b32d33abf9ed96e749e779efb0bb77bb6
antidmg <antidmg@proton.me>
2023-04-05 10:14:29 -0500

    add migration for auth_pubkey

---
+++ migrations/20230405142855_auth_pubkey.sql
@@ -0,0 +1,7 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS auth_pubkey(
+       user_id INT NOT NULL,
+       pubkey TEXT NOT NULL,
+       PRIMARY KEY (user_id, pubkey),
+       FOREIGN KEY (user_id) REFERENCES users(id),
+);
