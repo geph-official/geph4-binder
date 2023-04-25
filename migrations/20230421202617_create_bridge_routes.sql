e6a814e0fdc032e30ba361e7720f88acce38ebc7
antidmg <antidmg@proton.me>
2023-04-25 18:14:56 -0500

    update bridge_routes script
    Co-authored-by: sadministrator <marcothewebdev@gmail.com>

--- migrations/20230421202617_create_bridge_routes.sql
+++ migrations/20230421202617_create_bridge_routes.sql
@@ -1,7 +1,7 @@
 -- Create a replacement table for `routes` --
 CREATE TABLE public.bridge_routes (
-    exit_hostname      TEXT NOT NULL,
-    bridge_descriptor  BYTEA NOT NULL UNIQUE,
+    exit_hostname      TEXT NOT NULL UNIQUE,
+    descriptor  BYTEA NOT NULL UNIQUE,
     update_time        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     FOREIGN KEY (exit_hostname) REFERENCES exits(hostname) ON DELETE CASCADE
 );
