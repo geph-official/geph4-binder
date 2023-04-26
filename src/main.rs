25901af8d247816de7954d1bea9e5ce5ffd1251e
nullchinchilla <nullchinchilla@pm.me>
2023-04-26 15:16:19 -0500

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod serve;
 
 use env_logger::Env;
 
+use log::LevelFilter;
 use mimalloc::MiMalloc;
 
 use rand::Rng;
@@ -56,7 +57,22 @@ fn main() -> anyhow::Result<()> {
     });
 
     smolscale::block_on(async {
-        env_logger::Builder::from_env(Env::default().default_filter_or("geph4_binder=info")).init();
+        env_logger::Builder::new()
+            .format(|buf, record| {
+                use std::io::Write;
+                writeln!(
+                    buf,
+                    "{}:{} {} [{}] - {}",
+                    record.file().unwrap_or("unknown"),
+                    record.line().unwrap_or(0),
+                    chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                    record.level(),
+                    record.args()
+                )
+            })
+            .filter(Some("geph4_binder"), LevelFilter::Info)
+            .init();
+
         let opt = Opt::from_args();
 
         log::info!("geph4-binder starting with:");
