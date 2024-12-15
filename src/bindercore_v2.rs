960500ff77bfa1b332766c72fbf33b144f4f1d3a
nullchinchilla <nullchinchilla@pm.me>
2024-12-15 07:01:47 -0500

    Increase default POOL_SIZE multiplier from 3 to 8 in bindercore_v2.rs

--- src/bindercore_v2.rs
+++ src/bindercore_v2.rs
@@ -85,7 +85,7 @@ pub struct BinderCoreV2 {
 }
 
 pub static POOL_SIZE: LazyLock<u32> =
-    LazyLock::new(|| 3 * available_parallelism().unwrap().get() as u32);
+    LazyLock::new(|| 8 * available_parallelism().unwrap().get() as u32);
 
 impl BinderCoreV2 {
     /// Constructs a BinderCore.
